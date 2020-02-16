1ccde5d93c67aa2276abacbb02496fdee62b17b8
Bob Robey <brobey@lanl.gov>
2020-02-15 20:03:30 -0700

    Cleaning up example for book

--- DPCPP/StreamTriad/StreamTriad.cc
+++ DPCPP/StreamTriad/StreamTriad.cc
@@ -3,7 +3,8 @@ extern "C" {
    #include "timer.h"
 }  
 
-namespace sycl = cl::sycl;
+namespace Sycl = cl::sycl;
+using namespace std;
 
 int main(int argc, char * argv[])
 {
@@ -11,41 +12,45 @@ int main(int argc, char * argv[])
     double ttotal_sum = 0.0;
 
     size_t nsize = 10000;
-    std::cout << "StreamTriad with " << nsize << " elements" << std::endl;
+    cout << "StreamTriad with " << nsize << " elements" << endl;
 
     // host data
-    const double xval(1);
-    const double yval(2);
-    const double zval(2);
-    std::vector<double> a(nsize,xval);
-    std::vector<double> b(nsize,yval);
-    std::vector<double> c(nsize,zval);
+    vector<double> a(nsize,1.0);
+    vector<double> b(nsize,2.0);
+    vector<double> c(nsize,-1.0);
 
     cpu_timer_start(&ttotal);
 
-    sycl::queue q(sycl::default_selector{});
+    Sycl::queue Queue(sycl::default_selector{});
 
-    const double scalar = 3;
+    const double scalar = 3.0;
 
-    sycl::buffer<double,1> dev_a { a.data(), sycl::range<1>(a.size()) };
-    sycl::buffer<double,1> dev_b { b.data(), sycl::range<1>(b.size()) };
-    sycl::buffer<double,1> dev_c { c.data(), sycl::range<1>(c.size()) };
+    Sycl::buffer<double,1> dev_a { a.data(), Sycl::range<1>(a.size()) };
+    Sycl::buffer<double,1> dev_b { b.data(), Sycl::range<1>(b.size()) };
+    Sycl::buffer<double,1> dev_c { c.data(), Sycl::range<1>(c.size()) };
 
-    q.submit([&](sycl::handler& h) {
+    Queue.submit([&](sycl::handler& Handler) {
 
-       auto a = dev_a.get_access<sycl::access::mode::read>(h);
-       auto b = dev_b.get_access<sycl::access::mode::read>(h);
-       auto c = dev_c.get_access<sycl::access::mode::read_write>(h);
+       auto a = dev_a.get_access<Sycl::access::mode::read>(Handler);
+       auto b = dev_b.get_access<Sycl::access::mode::read>(Handler);
+       auto c = dev_c.get_access<Sycl::access::mode::write>(Handler);
 
-       h.parallel_for<class StreamTriad>( sycl::range<1>{nsize}, [=] (sycl::id<1> it) {
+       Handler.parallel_for<class StreamTriad>( Sycl::range<1>{nsize}, [=] (Sycl::id<1> it) {
            const int i = it[0];
-           c[i] += a[i] + scalar * b[i];
+           c[i] = a[i] + scalar * b[i];
        });
     });
-    q.wait();
+    Queue.wait();
+
+    for (int i=0, icount=0; i<nsize && icount < 10; i++){
+       if (c[i] != 1.0 + 3.0*2.0) {
+          cout << "Error with result c[" << i << "]=" << c[i] << endl;
+          icount++;
+       }
+    }
 
     ttotal_sum += cpu_timer_stop(ttotal);
 
-    std::cout << "Program completed without error." << std::endl;
-    std::cout << "Runtime is  " << ttotal_sum << " msecs " <<std::endl;
+    cout << "Program completed without error." << endl;
+    cout << "Runtime is  " << ttotal_sum << " msecs " << endl;
 }
