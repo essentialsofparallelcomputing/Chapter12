68ac631470a525a2905f8324fbcd0d5871cd0351
Bob Robey <brobey@lanl.gov>
2020-08-10 18:29:09 -0600

    Container fixes

--- dockerit.sh
+++ dockerit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker build -f Dockerfile.Ubuntu20.04 -t chapter12 .
-docker run -it --entrypoint /bin/bash chapter12
+#docker run --gpus all -it --entrypoint /bin/bash chapter12
+docker run -it --device=/dev/kfd --device=/dev/dri --group-add video -it --entrypoint /bin/bash chapter12
