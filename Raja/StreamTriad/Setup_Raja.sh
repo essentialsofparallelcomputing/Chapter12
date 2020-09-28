c3fe47bbb7c6a309d0012b91b5d1d573ad15f6c0
Bob Robey <brobey@lanl.gov>
2020-09-27 22:40:09 -0600

    Automating Raja build and test

---
+++ Raja/StreamTriad/Setup_Raja.sh
@@ -0,0 +1,18 @@
+# Raja
+
+rm -rf Raja build
+
+export INSTALL_DIR=`pwd`/Raja
+
+cd Raja_build
+rm -rf build
+mkdir build && cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+make -j 8 install
+cd ../..
+
+export Raja_DIR=${INSTALL_DIR}/share/raja/cmake
+
+mkdir build && cd build && cmake .. && make && ./StreamTriad
+
+cd .. && rm -rf build Raja
