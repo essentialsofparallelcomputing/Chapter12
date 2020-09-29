02fc027912233854beee8cbc4504fd7335a5f2df
Bob Robey <brobey@lanl.gov>
2020-09-28 21:31:49 -0600

    Fixing array initialization

--- CUDA/SumReduction/SumReduction.cu
+++ CUDA/SumReduction/SumReduction.cu
@@ -91,7 +91,7 @@ int main(int argc, char *argv[]){
 
    for (int i = 0; i<nsize; i++){
      //x[i] = rand()*100.0;
-     x[i] = 1.0;
+     x[i] = (double)i;
    }
 
    struct timespec tstart_cpu;
