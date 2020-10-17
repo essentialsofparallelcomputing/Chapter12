85dc1ee0591ca39ad081b0142cf365f7056e00ba
Bob Robey <brobey@lanl.gov>
2020-10-16 18:04:10 -0600

    Revising container installs

--- dockertest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker build --no-cache -t chapter12 .
-#docker run -it --entrypoint /bin/bash chapter12
-docker build --no-cache -f Dockerfile.Ubuntu20.04 -t chapter12 .
-#docker run -it --entrypoint /bin/bash chapter12
-docker build --no-cache -f Dockerfile.debian -t chapter12 .
-#docker run -it --entrypoint /bin/bash chapter12
