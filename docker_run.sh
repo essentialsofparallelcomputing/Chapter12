d720dcbcb1732f471ce424e32df9b3cf8a7eb91a
Bob Robey <brobey@lanl.gov>
2020-10-20 10:30:50 -0600

    Updating container

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-docker build -f Dockerfile.Ubuntu20.04 -t chapter12 .
-#docker run --gpus all -it --entrypoint /bin/bash chapter12
-docker run -it --device=/dev/kfd --device=/dev/dri --group-add video -it --entrypoint /bin/bash chapter12
+#docker build -f Dockerfile.Ubuntu20.04 -t chapter12 .
+#docker run --gpus all nvidia/cuda:11.0-base nvidia-smi
+docker run --rm --gpus all -it --entrypoint /bin/bash chapter12
+#docker run --device=/dev/dri --gpus all -it --entrypoint /bin/bash chapter12
+#docker run -it --device=/dev/dri --group-add video -it --entrypoint /bin/bash chapter12
