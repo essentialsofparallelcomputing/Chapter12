a989acb5e285b7ed1bb22f45396dc12fa3cfd32f
Bob Robey <brobey@lanl.gov>
2020-10-22 09:15:57 -0600

    Adding Raja, Kokkos, DPCPP containers

---
+++ dockerraja.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker build -f Dockerfile.Raja -t chapter12 .
+#docker run --gpus all nvidia/cuda:11.0-base nvidia-smi
+#docker run --rm --gpus all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,display,video,graphics -it --entrypoint /bin/bash chapter12
+docker run --rm --gpus all -it --entrypoint /bin/bash chapter12
+#docker run --device=/dev/dri --gpus all -it --entrypoint /bin/bash chapter12
+#docker run -it --device=/dev/dri --group-add video -it --entrypoint /bin/bash chapter12
