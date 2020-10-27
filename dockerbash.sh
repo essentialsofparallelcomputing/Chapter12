e69905c832dfc57978b86dfc27d8edfe22093f56
Bob Robey <brobey@lanl.gov>
2020-10-27 12:35:16 -0600

    Fixing containers

---
+++ dockerbash.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+docker run -it --gpus all -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
+	--security-opt seccomp=unconfined --cap-add=SYS_PTRACE \
+        --entrypoint /bin/bash essentialsofparallelcomputing/chapter11
