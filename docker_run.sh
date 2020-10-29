863bfd98b57fa4ab295fc02b48a0a035c97d6bdd
Bob Robey <brobey@lanl.gov>
2020-10-28 20:32:31 -0600

    Container fixes

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-docker build -f Dockerfile.Ubuntu20.04 -t chapter12 .
-#docker run --gpus all nvidia/cuda:11.0-base nvidia-smi
-#docker run --rm --gpus all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,display,video,graphics -it --entrypoint /bin/bash chapter12
-docker run --rm --gpus all -it --entrypoint /bin/bash chapter12
-#docker run --device=/dev/dri --gpus all -it --entrypoint /bin/bash chapter12
-#docker run -it --device=/dev/dri --group-add video -it --entrypoint /bin/bash chapter12
+dockerbash.sh
